d6f23b755b482ca729abb43a9bbe442697fe069f
Maryna <114282706+maryna-sh@users.noreply.github.com>
2023-02-01 20:54:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,123 @@
+document.querySelector("#item1 .portfolio-image").addEventListener("click", function() {
+  let image = this.cloneNode(true);
+  image.style.width = "100%";
+  image.style.height = "auto";
+
+  let imageModal = document.createElement("div");
+  imageModal.classList.add("modal");
+  imageModal.appendChild(image);
+  imageModal.style.display = "block";
+
+  document.body.appendChild(imageModal);
+
+  imageModal.addEventListener("click", function() {
+    document.body.removeChild(imageModal);
+  });
+});
+
+
+document.querySelector("#item2 .portfolio-image").addEventListener("click", function() {
+  let image = this.cloneNode(true);
+  image.style.width = "100%";
+  image.style.height = "auto";
+
+  let imageModal = document.createElement("div");
+  imageModal.classList.add("modal");
+  imageModal.appendChild(image);
+  imageModal.style.display = "block";
+
+  document.body.appendChild(imageModal);
+
+  imageModal.addEventListener("click", function() {
+    document.body.removeChild(imageModal);
+  });
+});
+
+
+document.querySelector("#item3 .portfolio-image").addEventListener("click", function() {
+  let image = this.cloneNode(true);
+  image.style.width = "100%";
+  image.style.height = "auto";
+
+  let imageModal = document.createElement("div");
+  imageModal.classList.add("modal");
+  imageModal.appendChild(image);
+  imageModal.style.display = "block";
+
+  document.body.appendChild(imageModal);
+
+  imageModal.addEventListener("click", function() {
+    document.body.removeChild(imageModal);
+  });
+});
+
+
+
+document.querySelector("#item4 .portfolio-image").addEventListener("click", function() {
+  let image = this.cloneNode(true);
+  image.style.width = "100%";
+  image.style.height = "auto";
+
+  let imageModal = document.createElement("div");
+  imageModal.classList.add("modal");
+  imageModal.appendChild(image);
+  imageModal.style.display = "block";
+
+  document.body.appendChild(imageModal);
+
+  imageModal.addEventListener("click", function() {
+    document.body.removeChild(imageModal);
+  });
+});
+
+document.querySelector("#item5 .portfolio-image").addEventListener("click", function() {
+  let image = this.cloneNode(true);
+  image.style.width = "100%";
+  image.style.height = "auto";
+
+  let imageModal = document.createElement("div");
+  imageModal.classList.add("modal");
+  imageModal.appendChild(image);
+  imageModal.style.display = "block";
+
+  document.body.appendChild(imageModal);
+
+  imageModal.addEventListener("click", function() {
+    document.body.removeChild(imageModal);
+  });
+});
+
+
+document.querySelector("#item6 .portfolio-image").addEventListener("click", function() {
+  let image = this.cloneNode(true);
+  image.style.width = "100%";
+  image.style.height = "auto";
+
+  let imageModal = document.createElement("div");
+  imageModal.classList.add("modal");
+  imageModal.appendChild(image);
+  imageModal.style.display = "block";
+
+  document.body.appendChild(imageModal);
+
+  imageModal.addEventListener("click", function() {
+    document.body.removeChild(imageModal);
+  });
+});
+
+document.querySelector("#item7 .portfolio-image").addEventListener("click", function() {
+  let image = this.cloneNode(true);
+  image.style.width = "100%";
+  image.style.height = "auto";
+
+  let imageModal = document.createElement("div");
+  imageModal.classList.add("modal");
+  imageModal.appendChild(image);
+  imageModal.style.display = "block";
+
+  document.body.appendChild(imageModal);
+
+  imageModal.addEventListener("click", function() {
+    document.body.removeChild(imageModal);
+  });
+});
